818703c9ed059dc804e6e39e2e1daf6735c953ff
razcohen <raz@icohens.com>
2021-07-10 15:34:31 +0300

    Added some comments to describe design intent
    Ability to persist state and reload it to the FSM
    
    Created a builder for better readability of the fsm

--- src/main/fsm.ts
+++ src/main/fsm.ts
@@ -1,3 +1,5 @@
+import { promises } from "fs";
+
 export interface Event {
     name: string;
     id: string;
@@ -5,38 +7,47 @@ export interface Event {
 
 export type EventHandler = (event: Event) => Promise<any>;
 
+// I have decided to delegate the state transitions to the machine level and not to the state level
+// This is because:
+// - transition between states is an abstraction of the machine and not the state itself
+// - makes the machine much more readable on creation
+// - allows to have some functionality with return value when handling an event
+
 export abstract class State {
-    // by default name of state is the class name
+    // the name of the state is its class name
     public get name(): string {
         return this.constructor.name;
     }
-
     // The state can register handlers to specific events:
     // a handler is a function that recieves an event (detected by string) and returns something
     public readonly handlers: { [eventName: string]: EventHandler } = {};
-    protected registerHandler(
-        eventName: string,
-        eventHandler: EventHandler
-    ): void {
-        this.handlers[eventName] = eventHandler;
+    constructor(handlers: { [eventName: string]: EventHandler }) {
+        this.handlers = handlers;
     }
 
     // The state will handle an event if it has a handler, otherwise it will skip
-    async handle(event: Event): Promise<any> {
+    public async handle(event: Event): Promise<any> {
         if (!(event.name in this.handlers)) return;
         return await this.handlers[event.name](event);
     }
 }
 
-export class FSM {
+export class Machine {
     public readonly states: { [stateName: string]: State };
     public readonly transitions: {
         [stateName: string]: { [eventName: string]: string };
     } = {};
-    private _currentState: State;
+    private _currentStateName: string;
 
     // fsm constructor recieves all the states and the initial state
-    constructor(states: State[], initialState: State) {
+    constructor(
+        states: State[],
+        transitions: {
+            [stateName: string]: { [eventName: string]: string };
+        },
+        initialStateName: string
+    ) {
+        // initial state must be in states
         this.states = states.reduce(
             (obj: { [stateName: string]: State }, state: State) => {
                 obj[state.name] = state;
@@ -44,23 +55,23 @@ export class FSM {
             },
             {}
         );
-        this._currentState = initialState;
-    }
-
-    // transitions are added dynamically
-    // more readable (could have been in the constructor as well)
-    // throws an error if any of the state names is invalid
-    registerTransition(
-        stateName: string,
-        eventName: string,
-        nextStateName: string
-    ) {
-        if (!(stateName in this.states))
-            throw new Error(`${stateName} is not a valid state`);
-        if (!(nextStateName in this.states))
-            throw new Error(`${nextStateName} is not a valid state`);
-        if (!this.transitions[stateName]) this.transitions[stateName] = {};
-        this.transitions[stateName][eventName] = nextStateName;
+        this.validateStateName(initialStateName);
+        this._currentStateName = initialStateName;
+        Object.entries(transitions).forEach(([stateName, transitions]) => {
+            Object.entries(transitions).forEach(
+                ([eventName, nextStateName]) => {
+                    if (!(stateName in this.states))
+                        throw new Error(`${stateName} is not a valid state`);
+                    if (!(nextStateName in this.states))
+                        throw new Error(
+                            `${nextStateName} is not a valid state`
+                        );
+                    if (!this.transitions[stateName])
+                        this.transitions[stateName] = {};
+                    this.transitions[stateName][eventName] = nextStateName;
+                }
+            );
+        });
     }
 
     // Main method of the FSM
@@ -68,15 +79,72 @@ export class FSM {
     // if there is no transition assigned from current state with this event, then no transition will occur
     async handle(event: Event): Promise<any> {
         const nextStateName =
-            event.name in this.transitions[this._currentState.name]
-                ? this.transitions[this._currentState.name][event.name]
-                : this._currentState.name;
-        const result = await this._currentState.handle(event);
-        this._currentState = this.states[nextStateName];
+            event.name in this.transitions[this._currentStateName]
+                ? this.transitions[this._currentStateName][event.name]
+                : this._currentStateName;
+        const result = await this.states[this._currentStateName].handle(event);
+        this._currentStateName = nextStateName;
         return result;
     }
 
-    public get currentState(): State {
-        return this._currentState;
+    public get currentStateName(): string {
+        return this._currentStateName;
+    }
+
+    private validateStateName(stateName: string) {
+        if (!Object.keys(this.states).includes(stateName))
+            throw new Error(
+                `Cannot set state ${stateName} must be in states ${Object.keys(
+                    this.states
+                )}`
+            );
+    }
+
+    public set currentStateName(stateName: string) {
+        this.validateStateName(stateName);
+        this._currentStateName = stateName;
+    }
+}
+
+export const saveState = async (fsm: Machine, path: string) => {
+    const state: string = fsm.currentStateName;
+    await promises.writeFile(path, state);
+};
+
+export const reloadState = async (fsm: Machine, path: string) => {
+    const state: string = (await promises.readFile(path)).toString();
+    fsm.currentStateName = state;
+};
+
+// Builder for better readability
+export class MachineBuilder {
+    public _states: State[] = [];
+    public readonly transitions: {
+        [stateName: string]: { [eventName: string]: string };
+    } = {};
+    public _initialStateName: string = "";
+    states(states: State[]): MachineBuilder {
+        this._states = states;
+        return this;
+    }
+    transition(
+        stateName: string,
+        eventName: string,
+        nextStateName: string
+    ): MachineBuilder {
+        if (!this.transitions[stateName]) this.transitions[stateName] = {};
+        this.transitions[stateName][eventName] = nextStateName;
+        return this;
+    }
+    initialStateName(initialStateName: string): MachineBuilder {
+        this._initialStateName = initialStateName;
+        return this;
+    }
+    build(): Machine {
+        return new Machine(
+            this._states,
+            this.transitions,
+            this._initialStateName
+        );
     }
 }
--- src/test/fsm.test.ts
+++ src/test/fsm.test.ts
@@ -1,5 +1,13 @@
 import { randomUUID } from "crypto";
-import { FSM, State, Event } from "../main/fsm";
+import { readFileSync, unlinkSync } from "fs";
+import {
+    Machine,
+    State,
+    Event,
+    saveState,
+    reloadState,
+    MachineBuilder,
+} from "../main/fsm";
 
 const eventGeneratorFunction = (eventName: string) => {
     return (): Event => {
@@ -10,50 +18,61 @@ const eventGeneratorFunction = (eventName: string) => {
 const event1Generator = eventGeneratorFunction("event1");
 const event2Generator = eventGeneratorFunction("event2");
 
-class State0 extends State {}
-class State1 extends State {}
+class State0 extends State {
+    constructor() {
+        super({});
+    }
+}
+class State1 extends State {
+    constructor() {
+        super({});
+    }
+}
 class State2 extends State {
     constructor() {
-        super();
-        this.registerHandler("event1", async (event: Event) => {
-            const result = "Event1 was fired 3 times in a row!";
-            console.log(result);
-            return result;
+        super({
+            event1: async (event: Event) => {
+                const result = "Event1 was fired 3 times in a row!";
+                console.log(result);
+                return result;
+            },
         });
     }
 }
 
-const initFSM = (): FSM => {
+const initFSM = (): Machine => {
     const state0 = new State0();
     const state1 = new State1();
     const state2 = new State2();
-    const fsm = new FSM([state0, state1, state2], state0);
-    fsm.registerTransition(state0.name, "event1", state1.name);
-    fsm.registerTransition(state1.name, "event1", state2.name);
-    fsm.registerTransition(state1.name, "event2", state0.name);
-    fsm.registerTransition(state2.name, "event1", state2.name);
-    fsm.registerTransition(state2.name, "event2", state0.name);
+    const fsm = new MachineBuilder()
+        .states([state0, state1, state2])
+        .initialStateName(state0.name)
+        .transition(state0.name, "event1", state1.name)
+        .transition(state1.name, "event1", state2.name)
+        .transition(state1.name, "event2", state0.name)
+        .transition(state2.name, "event1", state2.name)
+        .transition(state2.name, "event2", state0.name)
+        .build();
     return fsm;
 };
 
 describe("test FSM firing 3 events in a row", () => {
     it("should return a string", async () => {
         const fsm = initFSM();
-        // test the fsm
         let result;
 
         // should start from state0
-        expect(fsm.currentState).toBeInstanceOf(State0);
+        expect(fsm.currentStateName).toBe(State0.name);
 
         result = await fsm.handle(event1Generator());
         // after 1st event should return nothing and proceed to state1
         expect(result).toBeUndefined();
-        expect(fsm.currentState).toBeInstanceOf(State1);
+        expect(fsm.currentStateName).toBe(State1.name);
 
         result = await fsm.handle(event1Generator());
         // after 2nd event should return nothing and proceed to state2
         expect(result).toBeUndefined();
-        expect(fsm.currentState).toBeInstanceOf(State2);
+        expect(fsm.currentStateName).toBe(State2.name);
 
         result = await fsm.handle(event1Generator());
         // after 3rd event should return the string
@@ -68,31 +87,51 @@ describe("test FSM firing 3 events not in a row", () => {
         let result;
 
         // should start from state0
-        expect(fsm.currentState).toBeInstanceOf(State0);
+        expect(fsm.currentStateName).toBe(State0.name);
 
         result = await fsm.handle(event1Generator());
         // after 1st event1 should return nothing and proceed to state1
         expect(result).toBeUndefined();
-        expect(fsm.currentState).toBeInstanceOf(State1);
+        expect(fsm.currentStateName).toBe(State1.name);
 
         result = await fsm.handle(event2Generator());
         // after event2 is fired should return nothing and proceed to state0
         expect(result).toBeUndefined();
-        expect(fsm.currentState).toBeInstanceOf(State0);
+        expect(fsm.currentStateName).toBe(State0.name);
 
         result = await fsm.handle(event1Generator());
         // after 1st event1 should return nothing and proceed to state1
         expect(result).toBeUndefined();
-        expect(fsm.currentState).toBeInstanceOf(State1);
+        expect(fsm.currentStateName).toBe(State1.name);
 
         result = await fsm.handle(event1Generator());
         // after 2nd event1 should return nothing and proceed to state2
         expect(result).toBeUndefined();
-        expect(fsm.currentState).toBeInstanceOf(State2);
+        expect(fsm.currentStateName).toBe(State2.name);
 
         result = await fsm.handle(event2Generator());
         // after event2 is fired should return nothing and proceed to state0
         expect(result).toBeUndefined();
-        expect(fsm.currentState).toBeInstanceOf(State0);
+        expect(fsm.currentStateName).toBe(State0.name);
+    });
+});
+
+describe("test FSM persistence", () => {
+    it("should reload state successfully", async () => {
+        let fsm = initFSM();
+        expect(fsm.currentStateName).toBe(State0.name);
+        await fsm.handle(event1Generator());
+        expect(fsm.currentStateName).toBe(State1.name);
+
+        // persist state
+        await saveState(fsm, "testing-persistence");
+
+        // reload FSM
+        fsm = initFSM();
+        await reloadState(fsm, "testing-persistence");
+        expect(fsm.currentStateName).toBe(State1.name);
+
+        // cleanup
+        unlinkSync("testing-persistence");
     });
 });
